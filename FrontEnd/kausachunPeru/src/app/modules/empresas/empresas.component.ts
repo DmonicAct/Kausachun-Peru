3a3afa655aebbc7083806f9d6da385e01b676292
Patrick Figueroa <patrick@globalnick.com>
2020-05-30 15:24:23 -0500

    empresas pagina

--- FrontEnd/kausachunPeru/src/app/modules/empresas/empresas.component.ts
+++ FrontEnd/kausachunPeru/src/app/modules/empresas/empresas.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'empresas-component',
-  templateUrl: 'empresas.template.html',
-  styleUrls: ['empresas.template.scss']
+  selector: "empresas-component",
+  templateUrl: "empresas.template.html",
+  styleUrls: ["empresas.template.scss"],
 })
 export class EmpresasComponent implements OnInit {
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    console.log("INIT EMRPESAS");
   }
-
 }
-
