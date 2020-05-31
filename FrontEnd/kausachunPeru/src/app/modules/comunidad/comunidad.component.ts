66b1516869743676175a8dba08e4f8c04ffb2bb4
Patrick Figueroa <patrick@globalnick.com>
2020-05-30 20:39:44 -0500

    comunidad view

--- FrontEnd/kausachunPeru/src/app/modules/comunidad/comunidad.component.ts
+++ FrontEnd/kausachunPeru/src/app/modules/comunidad/comunidad.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'comunidad-component',
-  templateUrl: 'comunidad.template.html',
-  styleUrls: ['comunidad.template.scss']
+  selector: "comunidad-component",
+  templateUrl: "comunidad.template.html",
+  styleUrls: ["comunidad.template.scss"],
 })
 export class ComunidadComponent implements OnInit {
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    console.log("INIT COMUNIDAD");
   }
-
 }
-
