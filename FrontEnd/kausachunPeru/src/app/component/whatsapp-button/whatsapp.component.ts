c9c3a4b70d93330a3d0388ab218c55014b20737f
Patrick Figueroa <patrick@globalnick.com>
2020-05-31 14:47:50 -0500

    wsp done

--- FrontEnd/kausachunPeru/src/app/component/whatsapp-button/whatsapp.component.ts
+++ FrontEnd/kausachunPeru/src/app/component/whatsapp-button/whatsapp.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'whatsapp-component',
-  templateUrl: 'whatsapp.template.html',
-  styleUrls: ['whatsapp.template.scss']
-  
+  selector: "whatsapp-component",
+  templateUrl: "whatsapp.template.html",
+  styleUrls: ["whatsapp.template.scss"],
 })
 export class WhatsAppComponent implements OnInit {
-
-  nombre:string = "";
-  apellido:string = "";
-  public booleanFooter:Boolean=true;
-  ngOnInit() {
-  }
-  public onFloatClick () {
-    // do your magic
+  nombre: string = "";
+  apellido: string = "";
+  public booleanFooter: Boolean = true;
+  ngOnInit() {}
+  public onFloatClick() {
+    console.log("wspppppp");
   }
 }
-
