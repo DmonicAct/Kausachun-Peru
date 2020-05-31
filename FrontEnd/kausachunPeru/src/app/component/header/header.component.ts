2685cfa81ddddc80f183519e4d6757bf34c03022
DmonicAct <lck_23@hotmail.com>
2020-05-30 23:03:19 -0500

    commit

--- FrontEnd/kausachunPeru/src/app/component/header/header.component.ts
+++ FrontEnd/kausachunPeru/src/app/component/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilServices } from 'src/app/services/util.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'header-component',
   templateUrl: 'header.template.html',
-  styleUrls: ['header.template.scss']
+  styleUrls: ['header.template.scss'],
+  providers: [UtilServices]
 })
 export class HeaderComponent implements OnInit {
+
+  public booleanColor:Boolean=true;
+
+  constructor(private router:Router){
+
+  }
   ngOnInit() {
    
   }
--- FrontEnd/kausachunPeru/src/app/modules/campesinos/campesinos.component.ts
+++ FrontEnd/kausachunPeru/src/app/modules/campesinos/campesinos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'campesinos-component',
   templateUrl: 'campesinos.template.html',
-  styleUrls: ['campesinos.template.scss'],
+  styleUrls: ['campesinos.template.scss']
 })
 export class CampesinosComponent implements OnInit {
   public items=[];
